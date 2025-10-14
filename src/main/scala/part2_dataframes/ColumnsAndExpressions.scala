07558ff0fe7c51dc193f55384670b4b8b17bff38
marwhals <marjan.mubarok2012@gmail.com>
2025-10-14 21:34:02 +0100

    Columns and Expressions Excercises

--- src/main/scala/part2_dataframes/ColumnsAndExpressions.scala
+++ src/main/scala/part2_dataframes/ColumnsAndExpressions.scala
@@ -3,12 +3,14 @@ package part2_dataframes
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, column, expr}
 
-object ColumnsAndExpressions extends App{
+object ColumnsAndExpressions extends App {
 
   val spark = SparkSession.builder()
     .appName("DF Columns and Expressions")
     .config("spark.master", "local")
     .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
 
   val carsDF = spark.read
     .option("inferSchema", "true")
@@ -21,7 +23,9 @@ object ColumnsAndExpressions extends App{
   val carNamesDF = carsDF.select(firstColumn)
 
   // various select methods for selecting parts of a data frame
+
   import spark.implicits._
+
   carsDF.select(
     carsDF.col("Name"),
     col("Acceleration"),
@@ -35,7 +39,8 @@ object ColumnsAndExpressions extends App{
   carsDF.select("Name", "Year")
 
   /**
-   * Narrow transformation - After select we get a new DataFrame with less columns. Every input data frame has exactly one output partition in the dataframe. This is reflected across all clusters.
+   * Narrow transformation - After a "select" we get a new DataFrame with fewer columns.
+   * > Every input data frame has exactly one output partition in the dataframe. This is reflected across all clusters.
    */
 
   // Expressions: will allow you to process data frames in any way you like
@@ -75,7 +80,7 @@ object ColumnsAndExpressions extends App{
   val americanCarsDF = carsDF.filter("Origin = 'USA'")
   // chain filters
   val americanPowerfulCarsDF = carsDF.filter(col("Origin") === "USA").filter(col("Horsepower") > 150) //Triple equals for Spark
-  val americanPowerfulCarsDF2 = carsDF.filter(col("Origin") === "USA" and col("Horsepower") > 150)  // Chain filters. Infix for readability if required.
+  val americanPowerfulCarsDF2 = carsDF.filter(col("Origin") === "USA" and col("Horsepower") > 150) // Chain filters. Infix for readability if required.
   val americanPowerfulCarsDF3 = carsDF.filter("Origin = 'USA' and Horsepower > 150") // Pass filter in the same string
 
   // unioning = adding more rows
@@ -84,5 +89,62 @@ object ColumnsAndExpressions extends App{
 
   // distinct values
   val allCountriesDF = carsDF.select("Origin").distinct()
+
+  /**
+   * Excercies
+   * 1) - Read the movies DF and select 2 columns
+   * 2) - Create another column summing up the total profit of the movies i.e US_Gross + Worldwide_gross + DVD sales
+   * 3) - Select all comedy movies with IMDB rating above 6
+   */
+
+  val moviesDF = spark.read.option("inferSchema", "true").json("src/main/resources/data/movies.json")
+  moviesDF.show()
+
+  val moviesReleaseDF = moviesDF.select("Title", "Release_Date")
+  val moviesReleaseDF2 = moviesDF.select(
+    moviesDF.col("Title"),
+    col("Release_Date"),
+    $"Major_Genre",
+    expr("IMDB_Rating")
+  )
+  val moviesReleaseDF3 = moviesDF.selectExpr(
+    "Title", "Release_Date"
+  )
+// 1
+  moviesReleaseDF2.show()
+
+  val moviesProfitDF = moviesDF.select(
+    col("Title"),
+    col("US_Gross"),
+    col("Worldwide_Gross"),
+    col("US_DVD_Sales"),
+    (col("US_Gross") + col("Worldwide_Gross")).as("Total_Gross")
+  )
+
+  val moviesProfitDF2 = moviesDF.selectExpr(
+    "Title",
+    "US_Gross",
+    "Worldwide_Gross",
+    "US_Gross + Worldwide_Gross as Total_Gross"
+  )
+
+  val moviesProfitDF3 = moviesDF.select("Title", "US_Gross", "Worldwide_Gross")
+    .withColumn("Total_Gross", col("US_Gross") + col("Worldwide_Gross"))
+
+  // 2
+  moviesProfitDF3.show()
+
+  val atLeastMediocreComediesDF = moviesDF.select("Title", "IMDB_Rating")
+    .where(col("Major_Genre") === "Comedy" and col("IMDB_Rating") > 6)
+
+  val comediesDF2 = moviesDF.select("Title", "IMDB_Rating")
+    .where(col("Major_Genre") === "Comedy")
+    .where(col("IMDB_Rating") > 6)
+
+  val comediesDF3 = moviesDF.select("Title", "IMDB_Rating")
+    .where("Major_Genre = 'Comedy' and IMDB_Rating > 6")
+
+  // 3
+  comediesDF3.show
 
 }
